994cfd4423eeb1a9e4943ec4c842f640556ca3a2
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2022-05-26 21:46:22 -0400

    tailwind + routing + deployment

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rapid Reddit
+
+WIP Reddit client for desktop and mobile
+
+https://rapid-reddit.netlify.app/
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,57 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import { BrowserRouter, Link, Route, Routes, useParams } from 'react-router-dom'
 
 const Reddit = {
     getPosts: async function (sub: string) {
-        let x = await axios.get(`https://www.reddit.com/r/${sub}.json`, { params: { raw_json: 1 } })
+        let x = await axios.get(`https://www.reddit.com/r/${sub}/top.json`, { params: { raw_json: 1 } })
         return x.data
     },
     getPost: async function (url: string) {
-        let x = await axios.get(`https://www.reddit.com/${url}.json`, { params: { raw_json: 1 } })
+        let x = await axios.get(`https://www.reddit.com/${url}.json`, { params: { raw_json: 1, sort: 'top' } })
         return x.data
     }
 }
 
 export function App() {
-
-    let [posts, setPosts] = useState<any>()
-
-    let [post, setPost] = useState<any>()
-
-    useEffect(() => {
-        Reddit.getPosts('all').then(x => setPosts(x))
-    })
-    
+    document.querySelector('html')!.className = 'bg-black text-white'
     return <>
-        <div className='row'>
-            <div className='col'>
-                <Kind data={posts.data} kind={posts.kind} />
+        <BrowserRouter>
+            <div className='container mx-auto my-2 space-y-2'>
+                <Routes>
+                    <Route path='/' element={<Subreddit />} />
+                    <Route path='/r/:sub' element={<Subreddit />} />
+                    <Route path='/r/:sub/comments/:id/:any' element={<ViewPost />} />
+                </Routes>
             </div>
-            <div className='col'>
-                <Kind kind={post?.[1]?.kind} data={post?.[1]?.data} />
+        </BrowserRouter>
+    </>
+}
+
+function ViewPost() {
+
+    let { sub, id, any } = useParams()
+    let [post, setPost] = useState<any>()
+    useEffect(() => { Reddit.getPost(`r/${sub}/comments/${id}/${any}`).then(x => setPost(x)) }, [])
+
+    return <>
+        <div className='flex flex-col'>
+            <Kind kind={post?.[0]?.kind} data={post?.[0]?.data} />
+            <Kind kind={post?.[1]?.kind} data={post?.[1]?.data} />
+        </div>
+    </>
+}
+
+function Subreddit() {
+
+    let { sub } = useParams()
+    let [posts, setPosts] = useState<any>()
+    useEffect(() => { Reddit.getPosts(sub || 'all').then(x => setPosts(x)) }, [])
+
+    return <>
+        <div className='flex flex-row'>
+            <div className='flex flex-col space-y-2'>
+                <Kind data={posts?.data} kind={posts?.kind} />
             </div>
         </div>
     </>
@@ -49,22 +72,22 @@ function Kind({ kind, data }: any) {
 
 function Listing({ data }: any) {
     return <>
-        {data?.children?.map(x => <Kind data={x?.data} kind={x?.kind} />)}
+        {data?.children?.map((x: any, i: number) => <Kind key={i} data={x?.data} kind={x?.kind} />)}
     </>
 }
 
 function Post({ data }: any) {
     return <>
-        <div className='row'>
-            <div className='col'>
+        <div className='flex flex-row space-x-2 hover:text-red-400'>
+            <div className='flex flex-col'>
                 <img src={data?.thumbnail} alt='' />
             </div>
-            <div className='col'>
-                <div className='row'>
-                    <div> {data?.title} </div>
+            <div className='flex flex-col'>
+                <div className='flex flex-row'>
+                    <Link className='hover:text-blue-400' to={`${data?.permalink}`} > {data?.title} </Link>
                 </div>
-                <div className='row'>
-                    <div> r/{data?.subreddit} </div>
+                <div className='flex flex-row space-x-2'>
+                    <Link className='hover:text-blue-400' to={`/r/${data?.subreddit}`} > r/{data?.subreddit} </Link>
                     <div> u/{data?.author} </div>
                 </div>
             </div>
@@ -73,11 +96,15 @@ function Post({ data }: any) {
 }
 
 function Comment({ data }: any) {
+    let [fold, setFold] = useState<boolean>(false)
     return <>
-        <div>
-            <div> u/{data?.author} </div>
-            <div> {data?.body} </div>
-            {data?.replies && <Kind data={data?.replies?.data} kind={data?.replies?.kind} />}
+        <div className='m-2 border-l-[0.5px] border-white pl-2 hover:border-red-400'>
+            <div className='flex flex-row space-x-2'>
+                <div> u/{data?.author} </div>
+                <div className='text-sm'> {data?.score} </div>
+            </div>
+            <div className='hover:text-xl' onClick={() => setFold(!fold)}> {data?.body} </div>
+            {(data?.replies && !fold) && <Kind data={data?.replies?.data} kind={data?.replies?.kind} />}
         </div>
     </>
 }
