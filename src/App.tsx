040857639a7ba27814d3813be613d0b1895f0261
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2022-07-24 01:45:06 -0400

    make comment sort easier, hover hints, css tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { BrowserRouter, Link, Route, Routes, useParams } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes, useParams, useNavigate } from 'react-router-dom'
 import { Reddit } from './Reddit'
 import MarkDown from 'react-markdown'
 
@@ -31,15 +31,30 @@ export function App() {
         <BrowserRouter>
             <div className='container mx-auto'>
                 <Link to='/' className=''>
-                    <div className='p-8 w-full text-center text-2xl'> Rapid Reddit </div>
+                    <div className='p-8 w-full text-center text-2xl' onClick={() => updateState({ selected_subreddit: 'all' })} > Rapid Reddit </div>
                 </Link>
                 <Routes>
                     <Route path='/' element={<Subreddit state={state} updateState={updateState} />} />
+                    <Route path='/r/:subreddit' element={<SubRedirect state={state} updateState={updateState} />} />
                     <Route path='/:id' element={<ViewPost state={state} updateState={updateState} />} />
                 </Routes>
             </div>
         </BrowserRouter>
     </>
+}
+
+function SubRedirect({ state, updateState }: Props) {
+
+    let { subreddit } = useParams()
+    let nav = useNavigate()
+
+
+    useEffect(() => {
+        updateState({ selected_subreddit: subreddit || 'all' })
+        nav('/')
+    }, [])
+
+    return <></>
 }
 
 function Kind({ kind, data }: any) {
@@ -78,14 +93,14 @@ function Subreddit({ state, updateState }: Props) {
     return <>
         <div className='col'>
             <div className='row'>
-                <input type='text' defaultValue={state.selected_subreddit} onKeyDown={
+                <input type='text' className='bg p-4 outline-none w-full' placeholder='Subreddit' onKeyDown={
                     e => e.key === 'Enter' ? updateState({ selected_subreddit: e.currentTarget.value.replaceAll(' ', '') }) : null
                 } />
-                <select defaultValue={state.subreddit_sort} onChange={e => updateState({ subreddit_sort: e.currentTarget.value })}>
+                <select defaultValue={state.subreddit_sort} className='bg p-4 outline-none' onChange={e => updateState({ subreddit_sort: e.currentTarget.value })}>
                     {['hot', 'new', 'top'].map((x, i) => <option value={x} key={i}> {x} </option>)}
                 </select>
                 {state.subreddit_sort === 'top' && <>
-                    <select defaultValue={state.subreddit_sort_range} onChange={e => updateState({ subreddit_sort_range: e.currentTarget.value })}>
+                    <select defaultValue={state.subreddit_sort_range} className='bg p-4 outline-none' onChange={e => updateState({ subreddit_sort_range: e.currentTarget.value })}>
                         {['hour', 'day', 'week', 'month', 'year'].map((x, i) => <option value={x} key={i}> {x} </option>)}
                     </select>
                 </>}
@@ -103,17 +118,17 @@ function Post({ data }: any) {
 
     return <>
         <div className='col space-y-2 bg p-4'>
-            <Link to={`/${data?.id}`} className='col'>
-                <div className='row flex-wrap subtext text-sm space-x-2'>
+            <div className='col'>
+                <div className='row flex-wrap text-stone-400 text-sm space-x-2'>
                     <div> {data?.score} </div>
-                    <div> r/{data?.subreddit} </div>
+                    <Link to={`/r/${data?.subreddit}`} className='hover:text-stone-300'> r/{data?.subreddit} </Link>
                     <div> u/{data?.author} </div>
                     <div> {new Date(data?.created * 1000).toDateString().substring(4, 10)} </div>
                 </div>
-                <div className='row flex-wrap'>
+                <Link to={`/${data?.id}`} className='row flex-wrap hover:text-stone-300'>
                     <div> {data?.title} </div>
-                </div>
-            </Link>
+                </Link>
+            </div>
             {data?.thumbnail
                 ? <div className='row'>
                     {expanded
@@ -123,7 +138,7 @@ function Post({ data }: any) {
                         } alt='' className='rounded-xl' />
                     }
                 </div>
-                : <> {data?.selftext && <div className={expanded ? '' : 'subtext'} onClick={toggle}>
+                : <> {data?.selftext && <div className={expanded ? '' : 'text-stone-400'} onClick={toggle}>
                     {expanded
                         ? <MarkDown>{data?.selftext}</MarkDown>
                         : <MarkDown>{data?.selftext?.length > 300 ? data?.selftext?.substring(0, 250 - 3).padEnd(250, '.') : data?.selftext}</MarkDown>
@@ -141,7 +156,7 @@ function ThumbnailHandler({ data, toggle }: any) {
         return <>
             <div>
                 <video autoPlay controls src={data?.media?.reddit_video?.fallback_url} />
-                <span onClick={toggle} className='subtext'> Hide </span>
+                <span onClick={toggle} className='text-stone-400'> Hide </span>
             </div>
         </>
     }
@@ -158,7 +173,7 @@ function ThumbnailHandler({ data, toggle }: any) {
         return <>
             <div>
                 <video autoPlay controls src={`${data?.url}`.replace('.gifv', '.mp4')} />
-                <span onClick={toggle} className='subtext'> Hide </span>
+                <span onClick={toggle} className='text-stone-400'> Hide </span>
             </div>
         </>
     }
@@ -167,7 +182,7 @@ function ThumbnailHandler({ data, toggle }: any) {
         return <>
             <div>
                 <div dangerouslySetInnerHTML={{ __html: data?.media?.oembed?.html }} />
-                <span onClick={toggle} className='subtext'> Hide </span>
+                <span onClick={toggle} className='text-stone-400'> Hide </span>
             </div>
         </>
     }
@@ -207,10 +222,13 @@ function ViewPost({ state, updateState }: Props) {
             <Kind kind={post?.[0]?.kind} data={post?.[0]?.data} />
             {post
                 ? <>
-                    <div className='row'>
-                        <select defaultValue={state.post_sort} onChange={e => updateState({ post_sort: e.currentTarget.value })}>
-                            {['best', 'top', 'new', 'controversial'].map((x, i) => <option value={x} key={i}> {x} </option>)}
-                        </select>
+                    <div className='row p-1 pl-2 bg w-full space-x-2'>
+                        {['best', 'top', 'new', 'controversial'].map((x, i) =>
+                            <div
+                                className={`${state.post_sort === x ? 'text-stone-400' : 'hover:text-stone-400'}`}
+                                onClick={() => updateState({ post_sort: x })} key={i}
+                            > {x} </div>
+                        )}
                     </div>
                 </>
                 : <div className='loader'></div>}
@@ -226,7 +244,7 @@ function Comment({ data }: any) {
     return <>
         <div className='bg border-l-[0.5px] border-stone-400 pl-2'>
             <div className='p-2'>
-                <div onClick={() => setFold(!fold)} className='row subtext text-sm space-x-2'>
+                <div onClick={() => setFold(!fold)} className='row text-stone-400 text-sm space-x-2'>
                     <div> u/{data?.author} </div>
                     <div> {data?.score} </div>
                     <div> {new Date(data?.created * 1000).toDateString().substring(4, 10)} </div>
