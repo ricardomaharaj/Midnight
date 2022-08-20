e9de0a26783ed449a788291d86d8c57b785c4c1e
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2022-08-20 15:04:59 -0400

    add margin

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,23 @@ export function App() {
     return (
         <>
             <BrowserRouter>
-                <div className='container mx-auto my-4'>
-                    <div className='row m-2 text-xl justify-center'>
-                        <Link to='/'>Rapid Reddit</Link>
+                <div className='container mx-auto'>
+                    <div className='m-2'>
+                        <div className='row m-2 text-xl justify-center'>
+                            <Link to='/'>Rapid Reddit</Link>
+                        </div>
+                        <Routes>
+                            <Route path='/' element={<Subreddit />} />
+                            <Route
+                                path='/r/:subreddit'
+                                element={<Subreddit />}
+                            />
+                            <Route
+                                path='/r/:subreddit/:id'
+                                element={<Post />}
+                            />
+                        </Routes>
                     </div>
-                    <Routes>
-                        <Route path='/' element={<Subreddit />} />
-                        <Route path='/r/:subreddit' element={<Subreddit />} />
-                        <Route path='/r/:subreddit/:id' element={<Post />} />
-                    </Routes>
                 </div>
             </BrowserRouter>
         </>
